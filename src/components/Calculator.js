e5f1aa45447cb884057f4b40ff35e495cea1f150
lUCI <lusindisontanjana@gmail.com>
2022-10-28 09:00:33 +0200

    Add operation and calculator logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,21 +1,36 @@
-import React, { PureComponent } from 'react';
+import React, { Component } from 'react';
 import Buttons from './display/Buttons';
 import './Calculator.css';
 import Last from './display/Last';
 import Screen from './Screen';
+import calculate from '../logic/calculate';
 
-class Calculator extends PureComponent {
-  render() {
-    return (
-      <div className="calculator">
-        <Screen />
-        <div className="bottom">
-          <Last />
-          <Buttons />
-        </div>
-      </div>
-    );
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: 0,
+      next: 0,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
   }
-}
 
+	handleClick = (btnName) => {
+	  this.setState((prevState) => calculate(prevState, btnName));
+	};
+
+	render() {
+	  const { total, next, operation } = this.state;
+	  return (
+  <div className="calculator">
+    <Screen total={total} next={next} operation={operation} />
+    <div className="bottom">
+      <Last handleClick={this.handleClick} />
+      <Buttons handleClick={this.handleClick} />
+    </div>
+  </div>
+	  );
+	}
+}
 export default Calculator;
--- src/components/display/Buttons.js
+++ src/components/display/Buttons.js
@@ -1,18 +1,42 @@
 import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import Button from '../Button';
+
 import './Buttons.css';
 
-const list = ['AC', '+/-', '%', 7, 8, 9, 4, 5, 6, 1, 2, 3, 0, '.'];
+const list = [
+  'AC',
+  '+/-',
+  '%',
+  '7',
+  '8',
+  '9',
+  '4',
+  '5',
+  '6',
+  '1',
+  '2',
+  '3',
+  '0',
+  '.',
+];
 
 class Buttons extends PureComponent {
   render() {
-    const display = list.map((el) => (
-      <div className={`button ${el === 0 ? 'btn' : ''}`} key={el}>
-        {el}
-      </div>
-    ));
+    const { handleClick } = this.props;
 
-    return <div className="buttons">{display}</div>;
+    return (
+      <div className="buttons">
+        {list.map((el) => (
+          <Button value={el} key={el} handleClick={handleClick} />
+        ))}
+      </div>
+    );
   }
 }
+
+Buttons.propTypes = {
+  handleClick: PropTypes.func.isRequired,
+};
 
 export default Buttons;
