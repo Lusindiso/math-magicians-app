dd331344d48e081f2a00d95a82f23fa194d638cd
lUCI <lusindisontanjana@gmail.com>
2022-10-31 08:48:00 +0200

    Add pages with react-router

---
+++ src/components/display/pages/Home.jsx
@@ -0,0 +1,31 @@
+const Home = () => (
+	<main>
+		<h2 style={{
+			margin: '50px 10%',
+		}}
+		>
+			Welcome to our page!
+
+		</h2>
+		<p style={{
+			margin: '25px 10%',
+		}}
+		>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum pariatur
+			molestias officia, ducimus vitae voluptas asperiores porro provident, iure
+			et deserunt alias iste voluptatum dolorem maxime temporibus inventore. Quidem, odit.
+
+		</p>
+		<p style={{
+			margin: '25px 10%',
+		}}
+		>
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus accusantium
+			enim aliquid officia vero a cumque repellendus doloremque mollitia exercitationem
+			repudiandae suscipit, vitae, voluptatum eveniet tempora architecto alias quis aperiam?
+
+		</p>
+	</main>
+);
+
+export default Home;
---
+++ src/components/display/pages/Quote.jsx
@@ -0,0 +1,18 @@
+const Quote = () => (
+	<main style={{
+		width: '90vw',
+		display: 'flex',
+		justifyContent: 'center',
+		alignItems: 'center',
+		margin: '0 10%',
+		height: '90vh',
+	}}
+	>
+		<p>
+			Mathematics is not about numbers, equations, computations, or algorithms:
+			it is about understanding. - William Paul Thurston
+		</p>
+	</main>
+);
+
+export default Quote;
